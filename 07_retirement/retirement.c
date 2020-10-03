1f27f6cbf9c5a3f94afd921da6708e8aef9818f7
Coursera Learner <nobody@nowhere.nul>
2020-10-03 07:15:35 +0000

    Did assignment 7

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -5,7 +5,8 @@ struct _retire_info {
   int months;
   double contribution;
   double rate_of_return;
-}; typedef struct _retire_info retire_info;
+};
+typedef struct _retire_info retire_info;
 
 
 void retirement (int startAge,double initial,retire_info working, retire_info retired) {
