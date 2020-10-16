2059ee80de5803b3f279d92f31ef2a634b308e39
Coursera Learner <nobody@nowhere.nul>
2020-10-16 06:24:55 +0000

    deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -3,11 +3,13 @@
 #include <assert.h>
 #include "deck.h"
 
+/*
 int com (card_t c1,card_t c2){
   if ((c1.value == c2.value) &&(c1.suit == c2.suit)) return 1;
   return 0;
 
 }
+*/
 
 void print_hand(deck_t * hand){
   //card_t ** card =hand -> cards ;
@@ -19,40 +21,44 @@ void print_hand(deck_t * hand){
 }
 
 int deck_contains(deck_t * d, card_t c) {
-  card_t ** card =d -> cards ;
-  for (size_t i=0 ;i< d -> n_cards ;i++){
-    if (com(**(card+i),c)) return 1;
+  for (size_t i=0; i<(d->n_cards); i++) {
+    if (((d->cards[i]->value) == c.value) && ((d->cards[i]->suit)==c.suit)) {
+      //  printf("contained in deck");
+      return 1;
+    }
   }
-
   return 0;
 }
 
 void shuffle(deck_t * d){
-  card_t ** card =d -> cards ;
-  card_t * temp;
-  size_t n=d ->n_cards;
-  int randarry;
-  for (size_t i=0 ;i< n/2 ;i++){
-    randarry= random()%(n-i)+i;
-    temp=card[i];
-    card[i]=card[randarry];
-    card[randarry]=temp;
+  size_t max_rand=d->n_cards;
+  card_t swap_card;
+  size_t num;
+  size_t num2;
+  for (size_t i=0; i<max_rand; i++){
+    num=rand()%max_rand;
+    num2=rand()%max_rand;
+    //   while (num>max_rand){
+    //num/=2;
+    //}
+    // printf("num =%zu ", num);
+    swap_card.value=d->cards[num]->value;
+    swap_card.suit=d->cards[num]->suit;
+    d->cards[num]->value=d->cards[num2]->value;
+    d->cards[num]->suit=d->cards[num2]->suit;
+    d->cards[num2]->value=swap_card.value;
+    d->cards[num2]->suit=swap_card.suit;
   }
 }
 
 void assert_full_deck(deck_t * d) {
-  card_t ** card =d -> cards ;
-  card_t c;
-  int count;
-
-  for (size_t i=0 ;i< d -> n_cards ;i++){
-    c=**(card+i);
-    count=0;
-    for (size_t j=0 ;j< d -> n_cards ;j++){
-      if(com(**(card+j), c)) count ++;
-    }
-    assert(count ==1);
+  // printf("running assert_full_deck");
+  assert(d->n_cards == 52);
+  for (size_t i=0; i < d->n_cards; i++){
+    assert_card_valid(*(d->cards[i]));
+    assert(deck_contains(d, card_from_num(i))==1);
   }
+
 }
 
 
