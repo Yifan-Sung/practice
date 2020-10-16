0e35c07f6768be21e70cef7dde5fb330898d221c
Coursera Learner <nobody@nowhere.nul>
2020-10-16 06:10:41 +0000

    deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -10,11 +10,11 @@ int com (card_t c1,card_t c2){
 }
 
 void print_hand(deck_t * hand){
-  card_t ** card =hand -> cards ;
-  card_t  card1;
+  //card_t ** card =hand -> cards ;
+  //card_t  card1;
   for (size_t i=0 ;i<(hand -> n_cards );i++){
-    card1=**(card +i);
-    print_card(card1);
+    // card1=**(card +i);
+    print_card(*(hand->cards[i]));
   }
 }
 
