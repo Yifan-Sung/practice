a51f55e4927b25d6e6986bafb28d2af8f9bce43e
Coursera Learner <nobody@nowhere.nul>
2020-10-16 03:26:54 +0000

    deck

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -44,6 +44,7 @@ void assert_full_deck(deck_t * d) {
   card_t ** card =d -> cards;
   card_t c;
   int count;
+
   for (size_t i=0;i<d -> n_cards;i++){
     c=**(card+i);
     count=0;
@@ -54,6 +55,7 @@ void assert_full_deck(deck_t * d) {
   }
 }
 
+/*
 void add_card_to(deck_t * deck,card_t c){
   card_t * c1=malloc(sizeof(*c1));
   c1->value = c.value;
@@ -108,3 +110,5 @@ void free_deck(deck_t * deck){
   free(deck->cards);
   free(deck);
 }
+
+ */
